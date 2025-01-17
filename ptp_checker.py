683827eb18b7d304891fae94271a571136fe2a84
Abhishek F <95033966+notabhixhek@users.noreply.github.com>
2025-01-17 19:48:59 +0530

    Update ptp_checker.py

--- ptp_checker.py
+++ ptp_checker.py
@@ -8,4 +8,4 @@ def check_ptp(update: Update, context: CallbackContext):
         response.raise_for_status()
         update.message.reply_text("chal raha hai")
     except (requests.RequestException, requests.Timeout):
-        update.message.reply_text("gaya bhai") 
+        update.message.reply_text("gaya bhai")
